34d09b2609a98e52c6d5f7113e393a42e61c5931
Surfskills <ojjfred@gmail.com>
2023-04-02 00:23:36 +0300

    Step 5

--- app.py
+++ app.py
@@ -20,5 +20,17 @@ db = mysql.connector.connect(
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
