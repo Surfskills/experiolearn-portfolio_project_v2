107c7be73dab61a9449448ed3a6b1b6ac34cfa0a
Surfskills <ojjfred@gmail.com>
2023-04-02 12:49:18 +0300

    Step 6

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, render_template, request, redirect, url_for, session
+from flask_bootstrap import Bootstrap
 import mysql.connector
 import MySQLdb.cursors
 import re  
@@ -6,6 +7,8 @@ import re
 app = Flask(__name__, template_folder='templates')
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
 
 # Connect to MySQL database
 db = mysql.connector.connect(
@@ -20,6 +23,10 @@ db = mysql.connector.connect(
 def index():
     return render_template('index.html')
 
+@app.route('/home_page')
+def home_page():
+    return render_template('home_page.html')
+
 @app.route('/about')
 def about():
     return render_template('about.html')
@@ -28,9 +35,29 @@ def about():
 def register():
     return render_template('register.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/hub')
+def hub():
+    return render_template('hub.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/testimonial')
+def testimonial():
+    return render_template('testimonial.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
