3dc94a06d03eedbcfba7d9bf1e8e317ec024c0cb
Surfskills <ojjfred@gmail.com>
2023-04-05 14:39:04 +0300

    Not easy

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, session,flash 
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
-from uuid import uuid4
 import re
   
   
@@ -47,37 +46,27 @@ def logout():
   
 @app.route('/register', methods =['GET', 'POST'])
 def register():
-    message = ''
+    mesage = ''
     if request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'email' in request.form :
         userName = request.form['name']
         password = request.form['password']
         email = request.form['email']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        # Check if email already exists
         cursor.execute('SELECT * FROM user WHERE email = % s', (email, ))
         account = cursor.fetchone()
         if account:
-            message = 'Account already exists!'
+            mesage = 'Account already exists !'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            message = 'Invalid email address!'
+            mesage = 'Invalid email address !'
         elif not userName or not password or not email:
-            message = 'Please fill out the form!'
+            mesage = 'Please fill out the form !'
         else:
-            # Generate unique userid
-            while True:
-                userid = str(uuid4())
-                cursor.execute('SELECT * FROM user WHERE userid = %s', (userid,))
-                if not cursor.fetchone():
-                    break
-            # Insert new user
-            cursor.execute('INSERT INTO user VALUES (%s, %s, %s, %s)', (userid, userName, email, password))
+            cursor.execute('INSERT INTO user (name, email, password) VALUES (%s, %s, %s)', (userName, email, password,))
             mysql.connection.commit()
-            message = 'You have successfully registered!'
-            flash(message, 'success')
-            return redirect(url_for('login'))
+            mesage = 'You have successfully registered !'
     elif request.method == 'POST':
-        message = 'Please fill out the form!'
-    return render_template('register.html', message=message)
+        mesage = 'Please fill out the form !'
+    return render_template('register.html', mesage = mesage)
     
 if __name__ == "__main__":
     app.run()
